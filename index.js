6e558ad64e99bdee1a3552b83b64cc92cfd8be2a
Luis <lvas248@gmail.com>
2022-05-17 13:47:15 -0400

    commit tree d8d8457c7d714a15544e16b0ffcc2233548c3dd8

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.round(Math.random() * 100);
+random;
+
+let num3 = 4;
+let num4 = 8;
+let mod = num3 % num4;
+
+let max = Math.max(1,20,3,5);
+max;
